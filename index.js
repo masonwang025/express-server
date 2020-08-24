28d35ac9c77fce80dd3820db0725f81296abc4e3
MasonWang025 <mason.wang0025@gmail.com>
2020-08-24 14:05:14 -0700

    basic static folder as well as simple rest api

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const path = require("path");
+const members = require("./json-data/members.js");
 
 const app = express();
+
+// routes
+// simple REST api
+app.get("/api/members", (req, res) => {
+  res.json(members);
+});
+
+// set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000;
 
---
+++ json-data/members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: "Mason Wang",
+    email: "mason.wang0025@gmail.com",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "Jason Wang",
+    email: "jason.wang0025@gmail.com",
+    status: "active",
+  },
+  {
+    id: 3,
+    name: "Dason Wang",
+    email: "dason.wang0025@gmail.com",
+    status: "inactive",
+  },
+];
+
+module.exports = members;
