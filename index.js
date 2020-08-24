98cc974651797ba4100f4855011917d870feafca
MasonWang025 <mason.wang0025@gmail.com>
2020-08-24 15:09:22 -0700

    crud api complete

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const path = require("path");
-const logger = require("./middleware/logger");
+// const logger = require("./middleware/logger"); app.use(logger);
 
 const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // members api routes
 app.use("/api/members", require("./routes/api/members"));
--- routes/api/members.js
+++ routes/api/members.js
@@ -1,3 +1,5 @@
+const uuid = require("uuid");
+
 const express = require("express");
 
 const members = require("../../json-data/members.js");
@@ -12,6 +14,60 @@ router.get("/:id", (req, res) => {
   const found = members.some((member) => member.id === parseInt(req.params.id));
   if (found) {
     res.json(members.filter((member) => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
+
+// create member
+router.post("/", (req, res) => {
+  const newMember = {
+    id: uuid.v4(),
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  if (!newMember.name || !newMember.email) {
+    return res.status(400).json({ msg: "Please include a name and email." });
+  }
+
+  members.push(newMember);
+
+  res.json(members);
+});
+
+// update member
+router.put("/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+  if (found) {
+    const updMember = req.body;
+    members.forEach((member) => {
+      if (member.id === parseInt(req.params.id)) {
+        member.name = updMember.name || member.name;
+        member.email = updMember.email || member.email;
+
+        return res.json({
+          msg: `Member ${req.params.id} has been updated.`,
+          member,
+        });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+  if (found) {
+    newMembers = members.filter(
+      (member) => member.id !== parseInt(req.params.id)
+    );
+
+    res.json({
+      msg: `Member ${req.params.id} deleted`,
+      members: newMembers,
+    });
   } else {
     res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
   }
