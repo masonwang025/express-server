10f325ee3ec93e726beba4e6fba07c748f7e54f3
MasonWang025 <mason.wang0025@gmail.com>
2020-08-24 15:16:01 -0700

    express handlebars template rendering

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const path = require("path");
+const exphbs = require("express-handlebars");
 // const logger = require("./middleware/logger"); app.use(logger);
 
 const app = express();
 
-// middleware
+// handlebars middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// template rendering
+app.get("/", (req, res) => res.render("index")); //homepage route
 
 // members api routes
 app.use("/api/members", require("./routes/api/members"));
 
 // set static folder
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000;
 
